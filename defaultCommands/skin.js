daa847dee5a27a7eb71ac8fd5fbea11a82ecc76c
robloxxa <robloxxa@yandex.ru>
2021-07-25 12:50:08 +0300

    first commit

---
+++ defaultCommands/skin.js
@@ -0,0 +1,15 @@
+exports.conf = {
+    name: 'currentskin',
+    aliases: ['cs', 'skincurrent'],
+    modOnly: false,
+    regexp: {
+        only: false, // Set this to true if you don't wanna user call command within its name
+        value: '/skin?/g'
+    }
+}
+
+exports.run = async (client, channel, author, value) => {
+    const data = await client.gosu.data() //fetching data from gosumemory (./api/gosu.js)
+    if (!data) return client.twitch.say(channel, `Can't fetch data from streamer game`) // If no data recieved we return an error message
+    client.twitch.say(channel, `Current skin: ${data.settings.folders.skin}`)
+}
