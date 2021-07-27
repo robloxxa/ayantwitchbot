df325ade40b02e05c0129082aa735b782351199a
robloxxa <robloxxa@yandex.ru>
2021-07-27 16:52:48 +0300

    regexp value update

--- defaultCommands/osulink.js
+++ defaultCommands/osulink.js
@@ -4,7 +4,7 @@ exports.conf = {
     modOnly: false,
     regexp: {
         only: true,
-        value: /(?:http:\/\/|https:\/\/)?(?:osu|old).(?:gatari|ppy).(?:sh|pw)\/(?:b|s|beatmapsets)\/(\d*)#?(?:osu|mania|taiko|catch)?\/?(\d*)(?: |$|)/gi
+        value: /(?:http:\/\/|https:\/\/)?(?:osu|old).(?:gatari|ppy).(?:sh|pw)\/(b|s|beatmapsets)\/(\d*)#?(?:osu|mania|taiko|catch)?\/?(\d*) ?\+?(\w*)/gi
     }
 }
 
@@ -13,19 +13,20 @@ exports.run = async (client, channel, author, value) => {
         const joinedValue = value.join(" ")
         const regexp = new RegExp(this.conf.regexp.value)
         const link = regexp.exec(joinedValue)
-        const id = (link[2].length > 0) ? link[2] : link[1]
+        const id = (link[1] === 'beatmapsets') ? link[3] : link[2]
         const beatmaps = await client.bancho.osuApi.beatmaps.getByBeatmapId(id)
-        if (beatmaps.length < 1) return client.twitch.say('Wrong beatmap id')
+        if (beatmaps.length < 1) return client.twitch.say(channel, 'wrong beatmap id')
         const metadata = beatmaps[0]
-        const rankedStatus = metadata.approved // TODO: Need to make another function for parse rankedstatus like in !np command
+        const rankedStatus = client.getApiRankedStatus(metadata.approved)
+        const mods = (link[4].length > 2) ? client.parseMods(link[4]) : ''
+        const time = metadata.totalLength.toTime(true)
         await client.bancho.say(
-            `${author.username} > [${rankedStatus}] [https://osu.ppy.sh/b/${id} ${metadata.artist} - ${metadata.title} [${metadata.version}]] `
+            `${author.username} > [https://osu.ppy.sh/b/${id} [${rankedStatus}] ${metadata.artist} - ${metadata.title} [${metadata.version}] +${mods}] ${time}⏰ ${metadata.difficultyRating.toFixed(2)}✰ ${metadata.bpm.toFixed()}BPM`
         )
         return client.twitch.say(channel,
-            `[${rankedStatus}] ${metadata.artist} - ${metadata.title} [${metadata.version}] (by ${metadata.creator}) request added`)
+            `[${rankedStatus}] ${metadata.artist} - ${metadata.title} [${metadata.version}] +${mods} ${metadata.difficultyRating.toFixed(2)}✰ (by ${metadata.creator}) | request added`)
     } catch (e) {
         client.logger.error(e)
         return client.twitch.say('Something went wrong, request will not be send')
     }
-
 }
