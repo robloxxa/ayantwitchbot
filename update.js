77917ac3411ae64cc093aacc782763663411ec42
robloxxa <robloxxa@yandex.ru>
2021-08-03 09:14:33 +0300

    bug fixes and better version check

--- update.js
+++ update.js
@@ -3,9 +3,11 @@ const { version } = require('./package.json')
 const { writeFile , rename, readdir, rm } = require('fs/promises')
 const path = require('path')
 const { spawn } = require('child_process')
+const semver = require('semver')
 module.exports = async (client) => {
+    console.log()
     if (!process.argv.includes('CAXA')) return
-    await rm('./ayanbot-old').catch(() => {})
+    await rm(process.cwd()+'//ayanbot-old').catch(() => {})
     const tempDirPath = path.basename(path.join(__dirname+'/../'))
     const tempDirs = await readdir(__dirname + '/../../')
     for (const dir of tempDirs) {
@@ -13,9 +15,10 @@ module.exports = async (client) => {
         await rm(path.join(__dirname, '/../../', dir), { recursive: true })
     }
     client.logger.log('Checking for new updates')
-    const release = await axios.get('https://api.github.com/repos/robloxxa/ayantwitchbot/releases/latest')
-    if (release.data.tag_name === version) return client.logger.log('You have latest version ('+version+')')
-    if (!release.data || release.data.message === 'Not Found') return client.logger.log(`No response from github`)
+    const release = await axios.get('https://api.github.com/repos/robloxxa/ayantwitchbot/releases/latest').catch(() => {})
+    if (!release || release.data.message === 'Not Found') return client.logger.log(`No response from github`)
+    if (semver.satisfies(version, `>=${release.data.tag_name}`)) return client.logger.log('You have latest version ('+version+')')
+    if (!client.config.autoUpdate) return client.logger.log('AutoUpdate is off. New update available')
     const type = (process.platform === 'win32') ? 'application/vnd.microsoft.portable-executable' : 'application/octet-stream'
     const asset = release.data.assets.find(e => e.content_type === type)
     client.logger.log('Downloading new update')
