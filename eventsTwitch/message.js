05381fdee86236bc383b69d476d6639561ac29a2
robloxxa <robloxxa@yandex.ru>
2021-08-03 17:19:25 +0300

    added error handling in message event so cutsom commands will not crash the bot

--- eventsTwitch/message.js
+++ eventsTwitch/message.js
@@ -1,13 +1,17 @@
 module.exports = async (client, channel, author, message, self) => {
-    if (self) return
-    client.logger.debug(`${author.username}: ${message}`)
-    const regexp = (message.indexOf(client.config.prefix) === 0) ? false : client.getByRegexp(message)
-    if (message.indexOf(client.config.prefix) !== 0 && !regexp) return
-    const args = (regexp) ? message.trim().split(/ +/g) : message.slice(client.config.prefix.length).trim().split(/ +/g)
-    const command = (regexp) ? regexp : args.shift().toLowerCase()
-    const cmd = client.commands.get(command) || client.commands.get(regexp) || client.commands.get(client.aliases.get(command));
-    if (!cmd) return
-    if (cmd.conf.modOnly && (!author.mod || !author.badges.broadcaster)) return
-    if ((cmd.conf.regexp && cmd.conf.regexp.only) && cmd.conf.name === args[0]) return
-    cmd.run(client, channel, author, args)
+    try {
+        if (self) return
+        client.logger.debug(`(Twitch) ${author.username}: ${message}`)
+        const regexp = (message.indexOf(client.config.prefix) === 0) ? false : client.getByRegexp(message)
+        if (message.indexOf(client.config.prefix) !== 0 && !regexp) return
+        const args = (regexp) ? message.trim().split(/ +/g) : message.slice(client.config.prefix.length).trim().split(/ +/g)
+        const command = (regexp) ? regexp : args.shift().toLowerCase()
+        const cmd = client.commands.get(command) || client.commands.get(regexp) || client.commands.get(client.aliases.get(command));
+        if (!cmd) return
+        if (cmd.conf.modOnly && (!author.mod || !author.badges.broadcaster)) return
+        if ((cmd.conf.regexp && cmd.conf.regexp.only) && cmd.conf.name === args[0]) return
+        cmd.run(client, channel, author, args)
+    } catch (e) {
+        client.logger.error('(TwitchMessageEvent) '+e)
+    }
 }
