8a95da9b5fb9b86faa45008f4efd538ff5baa74b
robloxxa <robloxxa@yandex.ru>
2021-08-05 21:08:48 +0300

    Message timeout, this was made to prevent command spam, you can change timeout value in config.json (in ms)

--- eventsTwitch/message.js
+++ eventsTwitch/message.js
@@ -1,14 +1,18 @@
+let timeout = 0
+
 module.exports = async (client, channel, author, message, self) => {
     try {
         if (self) return
+        if (Date.now() - lasttimeout < client.config.timeout) return
+        timeout = Date.now()
         client.logger.debug(`(Twitch) ${author.username}: ${message}`)
         const regexp = (message.indexOf(client.config.prefix) === 0) ? false : client.getByRegexp(message)
         if (message.indexOf(client.config.prefix) !== 0 && !regexp) return
         const args = (regexp) ? message.trim().split(/ +/g) : message.slice(client.config.prefix.length).trim().split(/ +/g)
         const command = (regexp) ? regexp : args.shift().toLowerCase()
-        const cmd = client.commands.get(command) || client.commands.get(regexp) || client.commands.get(client.aliases.get(command));
-        if (!cmd) return
-        if (cmd.conf.modOnly && (!author.mod || !author.badges.broadcaster)) return
+        const cmd = client.commands.get(command) || client.commands.get(regexp) || client.commands.get(client.aliases.get(command))
+        if (author.badges && author.badges.broadcaster) author.mod = true
+        if (cmd.conf.modOnly && !author.mod) return
         if ((cmd.conf.regexp && cmd.conf.regexp.only) && cmd.conf.name === args[0]) return
         cmd.run(client, channel, author, args)
     } catch (e) {
