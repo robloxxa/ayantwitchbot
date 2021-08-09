d88c65e6dae7501939c60e89059ca1f8d1f8322a
robloxxa <robloxxa@yandex.ru>
2021-08-09 12:40:15 +0300

    Patch Notes is now more pretty in console

--- update.js
+++ update.js
@@ -4,6 +4,17 @@ const { writeFile , rename, readdir, rm } = require('fs/promises')
 const path = require('path')
 const { spawn } = require('child_process')
 const semver = require('semver')
+const chalk = require('chalk')
+const prettyNotes = (body) => {
+    return body
+        .replace(/(?:\#\# )([a-zA-Z0-9 ]*)/g, chalk.cyan('$1'))
+        .replace(/\\/g, ' ')
+        .replace(/(\*)/g, chalk.cyan('$1'))
+        .replace(/\>([\S ]*)/g, chalk.gray(' $1'))
+        .replace(/`([a-zA-Z0-9*: ]*)`/g, chalk.yellow('$1'))
+        .replace(/(?:\!\[([a-zA-Z0-9 ]*)\])\(([\S ]*)\)/g, '$2')
+}
+
 module.exports = async (client) => {
     if (!process.argv.includes('CAXA')) return
     await rm(process.cwd()+'//ayanbot-old').catch(() => {})
@@ -16,7 +27,7 @@ module.exports = async (client) => {
     client.logger.log('Checking for new updates')
     const release = await axios.get('https://api.github.com/repos/robloxxa/ayantwitchbot/releases/latest').catch(() => { return false })
     if (!release || release.data.message === 'Not Found' || !release.data.assets) return client.logger.log(`No response from github`)
-    if (semver.satisfies(version, `>=${release.data.tag_name}`)) return client.logger.log('You have latest version ('+version+')\n'+release.data.body)
+    if (semver.satisfies(version, `>=${release.data.tag_name}`)) return client.logger.log('You have latest version ('+version+')'+prettyNotes(release.data.body))
     if (!client.config.autoUpdate) return client.logger.log('New update available (AutoUpdate is off)\n'+release.data.body)
     const type = (process.platform === 'win32') ? 'application/vnd.microsoft.portable-executable' : 'application/octet-stream'
     const asset = release.data.assets.find(e => e.content_type === type)
